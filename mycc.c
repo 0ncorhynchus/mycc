c44cf8f133c85845071fa604ef947c2fdfb69009
Suguru Kato <salmon.bioinformatics@gmail.com>
2020-10-11 16:16:15 +0900

    :sparkles: Implement unary()

--- mycc.c
+++ mycc.c
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+// # EBNF
+//
+//  expr     =  mul ("+" mul | "-" mul)*
+//  mul      =  unary ( "*" unary | "/" unary)*
+//  unary    =  ("+" | "-")? primary
+//  primary  =  num | "(" expr ")"
+//
+
 typedef enum {
     TK_RESERVED,
     TK_NUM,
@@ -141,14 +149,22 @@ Node *primary() {
     return new_node_num(expect_number());
 }
 
+Node *unary() {
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), primary());
+    return primary();
+}
+
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
