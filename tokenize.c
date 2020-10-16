9947e467f86aef3b452e9e61d5678c455be8cb84
Suguru Kato <salmon.bioinformatics@gmail.com>
2020-10-16 23:37:43 +0900

    :truck: Split parse.c into parse.c and tokenize.c

---
+++ tokenize.c
@@ -0,0 +1,223 @@
+#include "mycc.h"
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const char *filename;
+static const char *user_input;
+
+const char *reserved[] = {"if",  "else",   "while", "for",  "return",
+                          "int", "sizeof", "char",  "void", NULL};
+
+void error_at(const Span *span, char *fmt, ...) {
+    const char *line = span->ptr;
+    while (user_input < line && line[-1] != '\n') {
+        line--;
+    }
+
+    const char *end = span->ptr;
+    while (*end != '\n') {
+        end++;
+    }
+
+    int line_num = 1;
+    for (const char *p = user_input; p < line; p++) {
+        if (*p == '\n') {
+            line_num++;
+        }
+    }
+
+    int indent = fprintf(stderr, "%s:%d: ", filename, line_num);
+    fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+    int pos = span->ptr - line + indent;
+    fprintf(stderr, "%*s", pos, "");
+    for (int i = 0; i < span->len; i++)
+        fprintf(stderr, "^");
+    fprintf(stderr, " ");
+
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+
+    exit(1);
+}
+
+static char *read_file(const char *path) {
+    FILE *fp = fopen(path, "r");
+    if (!fp) {
+        error("cannot open %s: %s", path, strerror(errno));
+    }
+
+    if (fseek(fp, 0, SEEK_END) == -1) {
+        error("%s: fseek: %s", path, strerror(errno));
+    }
+
+    size_t size = ftell(fp);
+    if (fseek(fp, 0, SEEK_SET) == -1) {
+        error("%s: fseek: %s", path, strerror(errno));
+    }
+
+    char *buf = calloc(1, size + 2);
+    fread(buf, size, 1, fp);
+
+    if (size == 0 || buf[size - 1] != '\n') {
+        buf[size++] = '\n';
+    }
+
+    buf[size] = '\0';
+    fclose(fp);
+    return buf;
+}
+
+static int is_alnum(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') ||
+           ('0' <= c && c <= '9') || (c == '_');
+}
+
+static Token *new_token(TokenKind kind, Token *cur, const char *str, int len) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->span.ptr = str;
+    tok->span.len = len;
+    cur->next = tok;
+    return tok;
+}
+
+const Token *tokenize(const char *path) {
+    filename = path;
+    user_input = read_file(filename);
+    const char *p = user_input;
+
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (strncmp(p, "//", 2) == 0) {
+            p += 2;
+            while (*p != '\n') {
+                p++;
+            }
+            continue;
+        }
+
+        if (strncmp(p, "/*", 2) == 0) {
+            char *q = strstr(p + 2, "*/");
+            if (!q) {
+                const Span span = {p, 1};
+                error_at(&span, "Unclosed comment");
+            }
+            p = q + 2;
+            continue;
+        }
+
+        if (*p == '=') {
+            int len;
+            if (*(p + 1) && *(p + 1) == '=')
+                len = 2;
+            else
+                len = 1;
+            cur = new_token(TK_RESERVED, cur, p, len);
+            p += len;
+            continue;
+        }
+
+        if (*p == '!') {
+            if (!*(p + 1) || *(p + 1) != '=') {
+                Span span = {p, 1};
+                error_at(&span, "Failed to tokenize");
+            }
+
+            cur = new_token(TK_RESERVED, cur, p, 2);
+            p += 2;
+            continue;
+        }
+
+        if (*p == '<' || *p == '>') {
+            int len;
+
+            if (*(p + 1) && *(p + 1) == '=') {
+                len = 2;
+            } else {
+                len = 1;
+            }
+
+            cur = new_token(TK_RESERVED, cur, p, len);
+            p += len;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+            *p == ')' || *p == ';' || *p == '{' || *p == '}' || *p == ',' ||
+            *p == '&' || *p == '[' || *p == ']') {
+            cur = new_token(TK_RESERVED, cur, p++, 1);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            const char *first = p;
+            char *endptr;
+            int val = strtol(p, &endptr, 10);
+            p = endptr;
+            int len = p - first;
+            cur = new_token(TK_NUM, cur, first, len);
+            cur->val = val;
+            continue;
+        }
+
+        if (*p == '"') {
+            const char *first = p;
+
+            p++; // consume begining '"'
+            while (*p != '"') {
+                p++;
+            }
+            p++; // consume ending '"'
+
+            int len = p - first;
+            cur = new_token(TK_STRING, cur, first, len);
+            continue;
+        }
+
+        if (('a' <= *p && *p <= 'z') || ('A' <= *p && *p <= 'Z') || *p == '_') {
+            const char *first = p;
+
+            p++;
+            while (is_alnum(*p)) {
+                p++;
+            }
+
+            int len = p - first;
+            TokenKind kind = TK_IDENT;
+
+            for (int i = 0; reserved[i]; i++) {
+                if (len == strlen(reserved[i]) &&
+                    strncmp(first, reserved[i], len) == 0) {
+                    kind = TK_RESERVED;
+                    break;
+                }
+            }
+
+            cur = new_token(kind, cur, first, len);
+            continue;
+        }
+
+        Span span = {p, 1};
+        error_at(&span, "Failed to tokenize");
+    }
+
+    new_token(TK_EOF, cur, p, 0);
+
+    return head.next;
+}
